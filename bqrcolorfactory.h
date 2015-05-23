c0cd984ba2e44cbef30a3c806ebc7b84d195abf6
kalebros <morgulero@gmail.com>
2015-05-23 20:15:59 +0200

    Added documentation

--- bqrcolorfactory.h
+++ bqrcolorfactory.h
@@ -5,15 +5,30 @@
 
 namespace BQREncode {
 
+/*!
+ * This factory builds a QR image code using a specific color as active pixel's color.
+ */
+
 class BQRColorFactory : public BQRImageFactory
 {
     Q_OBJECT
 public:
     explicit BQRColorFactory(QObject *parent=0,QColor color=QColor(Qt::black));
 
+    /*!
+     * Get the active pixel's color (black by default)
+     * @return Active pixel's color
+     * @note Doesn't modifies the object
+     */
     QColor getColor() const {
         return _color;
     }
+
+    /*!
+     * Set the active pixel's color (black by default)
+     * @param   color   Active pixel's color
+     * @note Modifies the object
+     */
 
     void setColor(QColor c) {
         _color=c;
